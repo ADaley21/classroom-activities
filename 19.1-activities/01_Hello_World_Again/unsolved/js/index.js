0be961eda57f43dbeac67d6cf61f66fb7ba848a9
ADaley21 <msalexandriad@gmail.com>
2021-09-25 10:24:24 -0400

    ..

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,46 @@
 // Create a variable below
 
+
+var myfirstVariable = 'This is my first variable'
+
+var alexandria='Hello World, I am Alexandria';
+
+
+
+
 // Console.log() your variable here.
+
+
+console.log(myfirstVariable);
+
+console.log(alexandria);
+
+
+
+var redhat=5;
+var bluehat=9;
+
+
+var total='Your total is' + (redhat + bluehat);
+
+
+alert (total);
+
+
+function addNumbers(){
+  var two=2;
+  var three=3;
+  var five= two + three;
+
+  console.log(five);
+
+}
+
+addNumbers();
+  var two='alexandria'
+  var three='daley';
+  var five = 'my name is'; + (two + three);
+
+  console.log(five);
+
+
--- 19.1-activities/03_Using_jQuery/unsolved/js/index.js
+++ 19.1-activities/03_Using_jQuery/unsolved/js/index.js
@@ -1,5 +1,12 @@
 // Use jQuery to target the <h1> tag and make it red.
 
+$("h1").css("color","red");
+
+$(".blue").css("background-color","yellow", "font-size";20px);
+
+$("addMe").html("hello")
+
+
 // Use jQuery to target the blue class and make it blue.
 
 // Use jQuery to target the addMe ID and change its HTML to Hello.
